db28118758f13c9afb19cc7aa08cd1357ab9fce3
Dinmukhammed Kambarov <52813950+din-mukhammed@users.noreply.github.com>
2024-03-16 18:51:42 +0600

    Update remap.lua

--- nvim/lua/din/remap.lua
+++ nvim/lua/din/remap.lua
@@ -12,10 +12,18 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-vim.keymap.set("n", "<leader>n", ":NERDTreeFocus<CR>")
-vim.keymap.set("n", "<C-n>", ":NERDTree<CR>")
-vim.keymap.set("n", "<F1>", ":NERDTreeToggle<CR>")
+-- vim.keymap.set("n", "<C-nf>", ":NERDTreeFocus<CR>")
+-- vim.keymap.set("n", "<C-n>", ":NERDTree<CR>")
+-- vim.keymap.set("n", "<C-n>", ":NERDTreeToggle<CR>")
+-- vim.keymap.set("n", "<leader>n", ":NERDTreeToggle %<CR>")
 vim.keymap.set("n", "<C-f>", ":NERDTreeFind<CR>")
 
-vim.api.nvim_set_keymap('n', '<F3>', ':NERDTreeToggle %<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', "<C-n>", ':NERDTreeToggle %<CR>', { noremap = true })
 
+vim.api.nvim_set_keymap("n", "E", "$", {noremap=false})
+vim.api.nvim_set_keymap("n", "B", "^", {noremap=false})
+vim.api.nvim_set_keymap("n", "WW", ":w!<enter>", {noremap=false})
+vim.api.nvim_set_keymap("n", "QQ", ":q!<enter>", {noremap=false})
+vim.api.nvim_set_keymap("n", "TT", ":TransparentToggle<CR>", {noremap=true})
+vim.api.nvim_set_keymap("n", "]t", ":tabnext<CR>", {noremap=true})
+vim.api.nvim_set_keymap("n", "[t", ":tabprevious<CR>", {noremap=true})
